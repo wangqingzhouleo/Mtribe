5aa68a1868f4c9febaa1d215110c131d50ddcf98
leo.wang <leo.wang@massive.co>
2018-11-16 11:41:47 +1100

    add framework

---
+++ mtribes/Example.swift
@@ -0,0 +1,16 @@
+//
+//  Example.swift
+//  mtribes
+//
+//  Created by Leo Wang on 16/11/18.
+//  Copyright © 2018 Massive. All rights reserved.
+//
+
+import Foundation
+
+public class Example {
+
+    public static func doSomething() {
+        print("This is a framework")
+    }
+}
