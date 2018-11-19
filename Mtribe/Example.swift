be0b94008b4d7e8cb9f23670e342b1ef2b1552c3
leo.wang <leo.wang@massive.co>
2018-11-19 12:16:48 +1100

    initial commit

---
+++ Mtribe/Example.swift
@@ -0,0 +1,18 @@
+//
+//  Example.swift
+//  Mtribe
+//
+//  Created by Leo Wang on 19/11/18.
+//  Copyright © 2018 Massive. All rights reserved.
+//
+
+import UIKit
+
+public class Example {
+
+    public static func doSomething() {
+        print("Did something")
+    }
+
+    public static let name = "Massive"
+}
