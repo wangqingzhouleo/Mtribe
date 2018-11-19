b61ace4e5419001bee493b5e7eedae0e00ac2123
leo.wang <leo.wang@massive.co>
2018-11-19 12:14:10 +1100

    Initial Commit

---
+++ Mtribe/Mtribe.h
@@ -0,0 +1,19 @@
+//
+//  Mtribe.h
+//  Mtribe
+//
+//  Created by Leo Wang on 19/11/18.
+//  Copyright © 2018 Massive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mtribe.
+FOUNDATION_EXPORT double MtribeVersionNumber;
+
+//! Project version string for Mtribe.
+FOUNDATION_EXPORT const unsigned char MtribeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mtribe/PublicHeader.h>
+
+
